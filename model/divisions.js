c72f82315853a3bf444045198a2a6fa3d0827a84
ILoveBallsdex <128934745+ILoveBallsdex@users.noreply.github.com>
2026-04-04 21:36:36 -0400

    Delete model directory

--- model/divisions.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const divisionSchema = new mongoose.Schema({
-  name: String,
-  emoji: String
-});
-
-module.exports = mongoose.model('Division', divisionSchema);
